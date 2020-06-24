5ddb06b6f1c62b4080240ac7cc1c8e5226479675
Mic_Perkins <38668110+MPireCafe@users.noreply.github.com>
2020-06-24 11:26:09 -0400

    Add files via upload
    My first HTML calculator attempt. I wanted to see if I could create it.

---
+++ Calculator.js
@@ -0,0 +1,26 @@
+function calc() {
+  var a = parseInt (document.querySelector("#value1").value);
+  var b = parseInt (document.querySelector("#value2").value);
+  var op = document.querySelector("#operator").value;
+  var calculate;
+
+  if (op == "add") {
+var calculate = a + b;
+ } else 
+ if (op == "min") {
+var calculate = a + b;
+ }
+ if (op == "div") {
+    var calculate = a / b;
+     } else 
+     if (op == "mul") {
+    var calculate = a * b;
+     }
+
+     console.log(calculate);
+ }
+ document.querySelector("#result").innerHTML = calculate;
+
+
+
+    
